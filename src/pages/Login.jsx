60b9525bf6904717696a73526ddcef804b04798b
Tammibriggs <Godsfirstbriggs@gmail.com>
2022-01-16 16:11:20 +0100

    login functionality

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,4 +1,7 @@
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import styled from "styled-components";
+import { login } from "../redux/apiCalls";
 import {mobile} from "../responsive";
 
 const Container = styled.div`
@@ -48,6 +51,11 @@ const Button = styled.button`
   color: white;
   cursor: pointer;
   margin-bottom: 10px;
+
+  &:disabled {
+    background-color: hsla(180, 100%, 25.098039215686274%, 0.3);
+    cursor: not-allowed;
+  }
 `;
 
 const Link = styled.a`
@@ -57,15 +65,31 @@ const Link = styled.a`
   cursor: pointer;
 `;
 
+const Error = styled.span`
+  color: red;
+`
+
 const Login = () => {
+  
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const dispatch = useDispatch()
+  const {isFetching, error} = useSelector(state => state.user)
+
+  const handleLogin = async(e) => {
+    e.preventDefault()
+    await login(dispatch, {username, password})
+  }
+
   return (
     <Container>
       <Wrapper>
         <Title>SIGN IN</Title>
         <Form>
-          <Input placeholder="username" />
-          <Input placeholder="password" />
-          <Button>LOGIN</Button>
+          <Input placeholder="username" type='name' onChange={(e) => setUsername(e.target.value)}/>
+          <Input placeholder="password" type='password' onChange={(e) => setPassword(e.target.value)}/>
+          <Button onClick={handleLogin} disabled={isFetching}>LOGIN</Button>
+          {error && <Error>Something went wrong</Error>}
           <Link>DO NOT YOU REMEMBER THE PASSWORD?</Link>
           <Link>CREATE A NEW ACCOUNT</Link>
         </Form>
--- src/pages/Success.js
+++ src/pages/Success.js
@@ -1,9 +1,5 @@
-import { useLocation } from "react-router"
-
 const Success = () => {
   
-  const location = useLocation()
-
   return (
     <div>
       This is the success page
