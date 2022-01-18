a4f2362f34487916d37673b0a4a308795c95e9e4
Tammibriggs <Godsfirstbriggs@gmail.com>
2022-01-18 09:26:44 +0100

    product list page

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import {
 import UserList from "./pages/userList/UserList";
 import User from "./pages/user/User";
 import NewUser from "./pages/newUser/NewUser";
+import ProductList from "./pages/productList/ProductList";
 
 function App() {
   return (
@@ -29,6 +30,15 @@ function App() {
               <User />
             </Route>
             <Route path='/newUser'>
+              <NewUser />
+            </Route>
+            <Route exact path='/products'>
+              <ProductList />
+            </Route>
+            <Route path='/product/:productId'>
+              <User />
+            </Route>
+            <Route path='/newProduct'>
               <NewUser />
             </Route>
           </Switch>
