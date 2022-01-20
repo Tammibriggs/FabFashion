2a37b542a84b2e9987446846acd260c0342f178d
Tammibriggs <Godsfirstbriggs@gmail.com>
2022-01-20 02:55:50 +0100

    minor changes

--- routes/user.js
+++ routes/user.js
@@ -44,7 +44,7 @@ router.get('/', verifyTokenAndAdmin, async (req, res) => {
   const query = req.query.new
   try{
     const users = query 
-    ? await User.find().sort({_id: -1}).limit(2) 
+    ? await User.find().sort({_id: -1}).limit(5) 
     : await User.find()
     res.status(200).json(users)
   }catch(err){ 
--- routes/verifyToken.js
+++ routes/verifyToken.js
@@ -6,7 +6,7 @@ const verifyToken = (req, res, next) => {
   if(authHeader){
     const token = authHeader.split(' ')[1]
     jwt.verify(token, process.env.JWT_SECT, (err, user) => {
-      if(err) res.status(403).json('Token is not valid')
+      if(err) return res.status(403).json('Token is not valid')
       req.user = user
       next()
     } )
